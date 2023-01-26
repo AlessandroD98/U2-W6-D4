bc2bc2e8df8125ab9e3693b1ecf87ca66d74bc29
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2023-01-26 17:46:35 +0100

    c

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ const call = async function () {
     if (DataFromUrl.ok) {
       let books = await DataFromUrl.json();
       console.log(books);
-      const threePartBooks = Math.ceil(books.length / 3);
-      const thirdPart = books.splice(-threePartBooks);
-      const secondPart = books.splice(-threePartBooks);
-      const firstPart = books;
-      let col1 = document.getElementById("col1");
-      let col2 = document.getElementById("col2");
-      let col3 = document.getElementById("col3");
-      firstPart.forEach((book) => {
-        col1.innerHTML =
-          col1.innerHTML +
+      let row1 = document.getElementById("row1");
+      books.forEach((book) => {
+        row1.innerHTML =
+          row1.innerHTML +
           `
+          <div class="col-4" >
           <div class="crad mb-3 border border-2" style="width: 18rem;">
           <img src="${book.img}"class="card-img-top" alt="">
           <div class="card-body">
@@ -23,33 +18,6 @@ const call = async function () {
           <p class="card-text">${book.price}€</p>
           <button type="button" class="btn btn-primary">Skip</button>
           </div>
-          </div>
-          `;
-      });
-      secondPart.forEach((book) => {
-        col2.innerHTML =
-          col2.innerHTML +
-          `
-          <div class="crad mb-3 border border-2" style="width: 18rem;">
-          <img src="${book.img}"class="card-img-top" alt="">
-          <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">${book.price}€</p>
-          <button type="button" class="btn btn-primary">Skip</button>
-          </div>
-          </div>
-          `;
-      });
-      thirdPart.forEach((book) => {
-        col3.innerHTML =
-          col3.innerHTML +
-          `
-          <div class="crad mb-3 border border-2" style="width: 18rem;">
-          <img src="${book.img}"class="card-img-top" alt="">
-          <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text">${book.price}€</p>
-          <button type="button" class="btn btn-primary">Skip</button>
           </div>
           </div>
           `;
@@ -57,7 +25,7 @@ const call = async function () {
       let buttons = document.querySelectorAll("button");
       buttons.forEach((a) => {
         a.addEventListener("click", () => {
-          a.parentElement.remove();
+          a.parentElement.parentElement.parentElement.remove();
         });
       });
     }
